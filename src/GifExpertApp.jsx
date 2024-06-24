8568f4ec26bf23f67a72d1057a7be416a56cf820
Brahyan Marquez <developermarquez93@gmail.com>
2024-06-24 17:52:16 -0500

    Creacion de componente addCategory

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { AddCategory } from './components/AddCategory';
+
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch','Dragon Ball' ]);
+
+    // console.log(categories);
+
+    const onAddCategory = () => {
+        setCategories([ 'Naruto', ...categories ]);
+        // setCategories([...categories, 'Naruto']);
+        // setCategories( cat => [...cat, 'Naruto'] );
+    }
+
+    return (
+        <>
+        {/* Titulo */}
+          <h1>GifExpertApp</h1>  
+
+        {/* Input de búsqueda Ingreso al sistema*/}
+        <AddCategory />
+
+        {/* listado de gif */}
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+            { 
+                categories.map(category => {
+                    return <li key={category}>{category}</li>
+            })
+            }
+        </ol>
+            {/* gif Item */}
+
+
+        </>
+    );
+}
